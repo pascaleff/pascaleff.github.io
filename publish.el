86d89c1a952f5f99e8e95ae4f37a94ab1413892e
James Pascaleff <jpascale@illinois.edu>
2022-06-22 16:16:55 -0500

    remove unnecessary package loading

--- publish.el
+++ publish.el
@@ -7,13 +7,13 @@
 
 ;;; Code:
 
-(require 'package)
-(package-initialize)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-refresh-contents)
-(package-install 'org-plus-contrib)
-(package-install 'htmlize)
+;; (require 'package)
+;; (package-initialize)
+;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; (package-refresh-contents)
+;; (package-install 'org-plus-contrib)
+;; (package-install 'htmlize)
 
 (require 'org)
 (require 'ox-publish)
